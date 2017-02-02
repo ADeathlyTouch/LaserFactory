ce70d7ecbca5f28511351d0de3ef06204fb25cb5
Collin Riggs <collinriggs@gmail.com>
2017-02-02 18:12:54 -0500

    Rotation fixed

--- src/main/java/com/collinriggs/laserfactory/blocks/lasers/LesserEnergyLaser.java
+++ src/main/java/com/collinriggs/laserfactory/blocks/lasers/LesserEnergyLaser.java
@@ -1,10 +1,19 @@
 package com.collinriggs.laserfactory.blocks.lasers;
 
-import net.minecraft.block.BlockRotatedPillar;
+import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class LesserEnergyLaser extends BlockRotatedPillar {
+public class LesserEnergyLaser extends BlockDirectional {
 
 	public LesserEnergyLaser() {
 		super(Material.PISTON);
@@ -14,6 +23,44 @@ public class LesserEnergyLaser extends BlockRotatedPillar {
 		this.setHardness(10);
 		this.setResistance(200);
 		this.setHarvestLevel("pickaxe", 3);
+
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.SOUTH));
 	}
+	
+	@Override
+    public IBlockState withRotation(IBlockState state, Rotation rot) {
+        return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
+    }
+	
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        IBlockState iblockstate = worldIn.getBlockState(pos.offset(facing.getOpposite()));
+
+        if (iblockstate.getBlock() == Blocks.END_ROD) {
+            EnumFacing enumfacing = (EnumFacing)iblockstate.getValue(FACING);
+
+            if (enumfacing == facing) {
+                return this.getDefaultState().withProperty(FACING, facing.getOpposite());
+            }
+        }
+
+        return this.getDefaultState().withProperty(FACING, facing);
+    }
+    
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        IBlockState iblockstate = this.getDefaultState();
+        iblockstate = iblockstate.withProperty(FACING, EnumFacing.getFront(meta));
+        return iblockstate;
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return ((EnumFacing)state.getValue(FACING)).getIndex();
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[] {FACING});
+    }
 	
 }
