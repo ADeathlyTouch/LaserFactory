577a1caea3c25874f80fd23127b599ca6a601b69
Collin Riggs <collinriggs@gmail.com>
2017-02-05 13:47:53 -0500

    TESR broken

--- src/main/java/com/collinriggs/laserfactory/blocks/misc/GlowingLapisTESR.java
+++ src/main/java/com/collinriggs/laserfactory/blocks/misc/GlowingLapisTESR.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 
 import org.lwjgl.opengl.GL11;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
@@ -15,7 +17,7 @@ public class GlowingLapisTESR extends TileEntitySpecialRenderer<GlowingLapisTE> 
 	@Override
 	public void renderTileEntityAt(GlowingLapisTE te, double x, double y, double z, float partialTicks, int destroyStage) {
 		super.renderTileEntityAt(te, x, y, z, partialTicks, destroyStage);
-		
+		if ((Integer)(int) x instanceof Integer) {
 		Color color = Color.RED;
 		
 		float red = color.getRed() / 255.0f;
@@ -23,37 +25,37 @@ public class GlowingLapisTESR extends TileEntitySpecialRenderer<GlowingLapisTE> 
         float blue = color.getBlue() / 255.0f;
         float alpha = color.getAlpha() / 255.0f;
 
-        GlStateManager.pushAttrib();
-        GlStateManager.pushMatrix();
-
         GlStateManager.translate(x, y, z);
+        
+        FontRenderer font = Minecraft.getMinecraft().fontRendererObj;
         
         Tessellator tessellator = Tessellator.getInstance();
         VertexBuffer buffer = tessellator.getBuffer();
-        
 
         buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
         
-        buffer.pos(0, 1, 0).color(red, green, blue, alpha).endVertex();
-        buffer.pos(0, 2, 0).color(red, green, blue, alpha).endVertex();
-        buffer.pos(1, 2, 0).color(red, green, blue, alpha).endVertex();
-        buffer.pos(1, 1, 0).color(red, green, blue, alpha).endVertex();
-
         buffer.pos(0, 1, 1).color(red, green, blue, alpha).endVertex();
         buffer.pos(0, 2, 1).color(red, green, blue, alpha).endVertex();
         buffer.pos(1, 2, 1).color(red, green, blue, alpha).endVertex();
         buffer.pos(1, 1, 1).color(red, green, blue, alpha).endVertex();
         
+        buffer.finishDrawing();
+        
+        
         GlStateManager.enableBlend();
         GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
         GlStateManager.disableLighting();
         GlStateManager.disableTexture2D();
+        GlStateManager.disableCull();
+        GlStateManager.pushAttrib();
+        GlStateManager.pushMatrix();
         tessellator.draw();
+        GlStateManager.popMatrix();
+        GlStateManager.popAttrib();
+        GlStateManager.enableCull();
         GlStateManager.enableTexture2D();
         GlStateManager.enableLighting();
         GlStateManager.disableBlend();
-        GlStateManager.popMatrix();
-        GlStateManager.popAttrib();
 	}
 	
 }
